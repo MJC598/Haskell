fd9afaa25b3ce8d0647b4a765700c42df42c8e60
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-10-24 15:07:09 -0500

    rpnParse Function

--- Homework4/HW4_mjc6r9.hs
+++ Homework4/HW4_mjc6r9.hs
@@ -6,10 +6,17 @@ data Op   = Val Int | Plus | Minus | Mul | IntDiv deriving (Show, Eq)
 type PExp = [Op] 
 
 rpnParse :: String -> PExp
-rpnParse = undefined
+rpnParse st = map(tester s) (words(st))
+    where tester :: String -> Op
+          tester "+" = Plus
+          tester "-" = Minus
+          tester "*" = Mul
+          tester "/" = IntDiv
+          tester s   = Val read s
 
 eval :: PExp -> Int
-eval = undefined
+eval []    = error "Bad Input"
+eval p:ps  = if  
 
 evalSafe :: PExp -> RPNResult
 evalSafe = undefined
