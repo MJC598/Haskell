7bfe8fab0dbaf55d397f104c1efab1417cb1ead0
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-10-24 20:27:09 -0500

    commenting code

--- Homework4/HW4_mjc6r9.hs
+++ Homework4/HW4_mjc6r9.hs
@@ -5,6 +5,7 @@ module HW4 where
 data Op   = Val Int | Plus | Minus | Mul | IntDiv deriving (Show, Eq)
 type PExp = [Op] 
 
+--parse the string into a list of Ops
 rpnParse :: String -> PExp
 rpnParse st = map(tester s) (words(st))
     where tester :: String -> Op
@@ -14,18 +15,26 @@ rpnParse st = map(tester s) (words(st))
           tester "/" = IntDiv
           tester s   = Val read s
 
+--evaluate the function after parsing
 eval :: PExp -> Int
 eval []  = error "Bad Input"
 eval ps  = head . foldl folding []
-        where folding    :: PExp -> Op -> PExp 
+        where folding :: PExp -> Op -> PExp 
               folding (x:y:ps) Plus   = (x + y):ys 
               folding (x:y:ps) Minus  = (x - y):ys
               folding (x:y:ps) Mul    = (x ** y):ys
               folding (x:y:ps) IntDiv = (x / y):ys
               folding ps Val Int      = p:ps 
 
-evalSafe :: PExp -> RPNResult
-evalSafe = undefined
+--new data sets and types
+data RPNError   = DivByZero | InvalidInput deriving (Show, Eq)
+data Either a b = Left a | Right b deriving (Show, Eq)
+type RPNResult  = Either RPNError Int
 
-rpnTranse :: PExp -> String
-rpnTranse = undefined
+--the safe evaulation function
+evalSafe :: PExp -> RPNResult
+evalSafe [] = InvalidInput
+
+--translation into infix
+rpnTrans :: PExp -> String
+rpnTrans = undefined
