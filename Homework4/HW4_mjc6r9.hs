768fb042c31f673a4ce7e5f64f3621fd06a2b3c3
Matthew Carroll <matthew.j.carroll@outlook.com>
2018-10-26 10:45:36 -0500

    Problems 1, 2, 4 are working. Debugging 3

--- Homework4/HW4_mjc6r9.hs
+++ Homework4/HW4_mjc6r9.hs
@@ -2,13 +2,6 @@ module HW4 where
 
 --Due: Friday October 26, by 3pm
 
--- solveRPN :: String -> Float
--- solveRPN = head . foldl foldingFunction [] . words
---     where   foldingFunction (x:y:ys) "*"    = (x * y):ys
---             foldingFunction (x:y:ys) "+"    = (x + y):ys
---             foldingFunction (x:y:ys) "-"    = (x - y):ys
---             foldingFunction xs numberString = read numberString:xs
-
 data Op   = Val Int | Plus | Minus | Mul | IntDiv deriving (Show, Eq)
 type PExp = [Op] 
 
@@ -38,28 +31,30 @@ eval xs = intConverter(head(foldl foldingFunction [] xs))
 foldingFunction :: PExp -> Op -> PExp
 foldingFunction (x:y:ys) Plus   = (Val(intConverter(x) + intConverter(y))):ys
 foldingFunction (x:y:ys) Minus  = (Val(intConverter(y) - intConverter(x))):ys
-foldingFunction (x:y:ys) Mul    = (Val(intConverter(y) * intConverter(x))):ys
-foldingFunction (x:y:ys) IntDiv = (Val(intConverter(x) `quot` intConverter(y))):ys
+foldingFunction (x:y:ys) Mul    = (Val(intConverter(x) * intConverter(y))):ys
+foldingFunction (x:y:ys) IntDiv = (Val(intConverter(y) `quot` intConverter(x))):ys
 foldingFunction xs numberString = numberString:xs
 
+--helper function for conversion purposes
 intConverter :: Op -> Int
 intConverter (Val x) = x 
 
--- --the safe evaulation function
--- evalSafe :: PExp -> RPNResult
--- evalSafe [] = InvalidInput
--- evalSafe ps = head . foldl folding [] ps
---             where folding :: PExp -> Op -> PExp
---                   folding (x:y:ps) Plus           = (x + y):ps
---                   folding (x:y:ps) Minus          = (x - y):ps
---                   folding (x:y:ps) Mul            = (x * y):ps
---                   folding (x:y:ps) IntDiv         = if y == 0
---                                                     then DivByZero
---                                                     else (x / y):ps
---                   -- folding ((x:ps) (Val (read x))) = x:ps
 --new data sets and types
 data RPNError   = DivByZero | InvalidInput deriving (Show, Eq)
 type RPNResult  = Either RPNError Int
+
+--PROBLEM 3
+
+evalSafe :: PExp -> RPNResult
+evalSafe [] = Left InvalidInput
+evalSafe xs = Right (intConverter(head(foldl foldFunction [] xs)))
+
+foldFunction :: PExp -> Op -> PExp
+foldFunction (x:y:ys) Plus   = (Val(intConverter(x) + intConverter(y))):ys
+foldFunction (x:y:ys) Minus  = (Val(intConverter(y) - intConverter(x))):ys
+foldFunction (x:y:ys) Mul    = (Val(intConverter(x) * intConverter(y))):ys 
+foldFunction (x:y:ys) IntDiv = (Val(intConverter(y) `quot` intConverter(x))):ys
+foldFunction xs numberString = numberString:xs
 
 --PROBLEM 4
 
